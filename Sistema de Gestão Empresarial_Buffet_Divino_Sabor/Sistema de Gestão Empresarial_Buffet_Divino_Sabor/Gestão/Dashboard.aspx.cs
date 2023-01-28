154e93928f5a4eaadb8e7fc4e1a58251e3a97765
LAB201 <LAB201@SERVEDUC.COM>
2023-01-27 21:28:03 -0300

    adicionei o campo turno em classe

--- Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Gestão/Dashboard.aspx.cs
+++ Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Gestão/Dashboard.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,10 @@ namespace Sistema_de_Gestão_Empresarial_Buffet_Divino_Sabor.Gestão
 {
     public partial class Dashboard : System.Web.UI.Page
     {
+        private MySqlConnection conexao;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            conexao = new MySqlConnection(SiteMaster.ConnectionString);
         }
 
         protected void grd_Escala_RowCommand(object sender, GridViewCommandEventArgs e)
