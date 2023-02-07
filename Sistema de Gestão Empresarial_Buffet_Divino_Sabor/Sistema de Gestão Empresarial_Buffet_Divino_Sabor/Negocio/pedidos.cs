b1dec7e945aa1a5faf772c084db2eb66bec31d75
LAB201 <LAB201@SERVEDUC.COM>
2023-02-06 22:11:36 -0300

    Finalização das funcionalidades da pagina de pedidos e concerto das páginas faltando

--- Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Negocio/pedidos.cs
+++ Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Negocio/pedidos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,48 @@ namespace Sistema_de_Gestão_Empresarial_Buffet_Divino_Sabor.Negocio
 {
     public class pedidos
     {
+        private MySqlConnection connection;
+        public pedidos()
+        {
+            connection = new MySqlConnection(SiteMaster.ConnectionString);
+        }
+
+        public bool Acept(Classe.pedidos pedidos)
+        {
+            try
+            {
+                connection.Open();
+                var comando = new MySqlCommand($@"UPDATE pedidos SET estado = 1 WHERE id = @id", connection);
+                comando.Parameters.Add(new MySqlParameter("id", pedidos.id));
+                comando.ExecuteNonQuery();
+                connection.Close();
+
+                
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool Finish(Classe.pedidos pedidos)
+        {
+            try
+            {
+                connection.Open();
+                var comando = new MySqlCommand($@"UPDATE pedidos SET estado = 0 WHERE id = @id", connection);
+                comando.Parameters.Add(new MySqlParameter("id", pedidos.id));
+                comando.ExecuteNonQuery();
+                connection.Close();
+
+
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
