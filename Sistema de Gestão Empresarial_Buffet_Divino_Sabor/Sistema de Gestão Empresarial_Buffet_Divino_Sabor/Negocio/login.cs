5fc851e55b9e6bd76da6a81802f05fe422ff8de1
LAB201 <LAB201@SERVEDUC.COM>
2023-02-17 21:47:32 -0300

    Implementações de Login na Pasta Negócios e Teste Unitário

---
+++ Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Negocio/login.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySqlConnector;
+
+namespace Sistema_de_Gestão_Empresarial_Buffet_Divino_Sabor.Negocio
+{
+    public class login
+    {
+        private MySqlConnection conexao;
+
+        public login()
+        {
+            conexao = new MySqlConnection(SiteMaster.ConnectionString);
+        }
+
+        public bool Create(Classe.Login login)
+        {
+            try
+            {
+               
+                //Criar Cadastro
+                conexao.Open();
+                var comando = new MySqlCommand("INSERT INTO login (email, senha) VALUES (@email,MD5(@senha))", conexao);
+                comando.Parameters.Add(new MySqlParameter("email", login.email));
+                comando.Parameters.Add(new MySqlParameter("senha", login.senha));
+                comando.ExecuteNonQuery();
+                conexao.Close();
+                return true;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+
+        public bool Read(Classe.Login login)
+        {
+            try
+            {
+                conexao.Open();
+                var comando = new MySqlCommand($"SELECT senha, email FROM login WHERE email = @email and senha = MD5(@senha) ", conexao);
+                comando.Parameters.Add(new MySqlParameter("email", login.email));
+                comando.Parameters.Add(new MySqlParameter("senha", login.senha));
+                var reader = comando.ExecuteReader();
+                if (reader.Read())
+                {
+                    return true;
+                    
+                }
+                
+                else
+                {
+                    return false;
+                }          
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }
+    }
+}
---
+++ Sistema de Gestão Empresarial_Buffet_Divino_Sabor/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var emails = new string[]
+            {
+                "fulano@email.com",
+                "sicrano@email.com",
+                "beltrano@email.com",
+                "deltrano@email.com",
+                "fulano001@email.com"
+            };
+            var senhas = new string[]
+            {
+                "fulano123456",
+                "sicrano123456",
+                "beltrano123456",
+                "deltrano123456",
+                "fulano00123456"
+            };
+
+            for(int i=0; i<emails.Length;i++)
+            {
+                var login = new Sistema_de_Gestão_Empresarial_Buffet_Divino_Sabor.Classe.Login();
+                login.email = emails[i];
+                login.senha = senhas[i];
+
+                var resultado = new Sistema_de_Gestão_Empresarial_Buffet_Divino_Sabor.Negocio.login().Create(login);
+                Assert.AreEqual(true, resultado);
+
+                var leitura = new Sistema_de_Gestão_Empresarial_Buffet_Divino_Sabor.Classe.Login();
+                leitura.email = emails[i];
+                leitura.senha = senhas[i];
+
+                var Login_bd = new Sistema_de_Gestão_Empresarial_Buffet_Divino_Sabor.Negocio.login().Read(leitura);
+                Assert.AreEqual(true, Login_bd);
+            }
+
+
+            
+
+            
+        }
+    }
+}
