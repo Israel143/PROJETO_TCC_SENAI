23f25861484bf0e898a427c6dbfeae9fddb23169
LAB201 <LAB201@serveduc.com>
2023-02-24 22:12:26 -0300

    Alteração do local do código do back end para a classe da pasta Negocios -- Página de pedidos

--- Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Gestão/Pedidos/Pedidos.aspx.cs
+++ Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Gestão/Pedidos/Pedidos.aspx.cs
@@ -16,49 +16,19 @@ namespace Sistema_de_Gestão_Empresarial_Buffet_Divino_Sabor.Gestão.Pedidos
         protected void Page_Load(object sender, EventArgs e)
         {
             conn = new MySqlConnection(SiteMaster.ConnectionString);
-            if (IsPostBack)
-            {
 
+                //Carrega os pedidos novos
+                repeaterOrders.DataSource = new Negocio.pedidos().ReadNovos();
+                repeaterOrders.DataBind();
 
-            }
+                //Carrega os pedidos em andamento
+                repeaterOrdersAcepted.DataSource = new Negocio.pedidos().ReadAndamento();
+                repeaterOrdersAcepted.DataBind();
 
-            conn.Open();
-            string query = "SELECT pedidos.id, pedidos.data, cliente.nome, menu.entrada, menu.salada, menu.jantar, menu.sobremesa, menu.bebida FROM pedidos INNER JOIN cliente ON pedidos.id_cliente = cliente.id INNER JOIN menu ON pedidos.id_menu = menu.id WHERE estado IS NULL";
-            using (MySqlCommand command = new MySqlCommand(query, conn))
-            {
-                using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
-                {
-                    DataTable dt = new DataTable();
-                    adapter.Fill(dt);
-                    repeaterOrders.DataSource = dt;
-                    repeaterOrders.DataBind();
-                }
-                conn.Close();
+                //Carrega os pedidos finalizados
+                repeaterFinishedOrders.DataSource = new Negocio.pedidos().ReadFinalizados();
+                repeaterFinishedOrders.DataBind();
 
-                conn.Open();
-                string query1 = "SELECT pedidos.id, pedidos.data, cliente.nome, menu.entrada, menu.salada, menu.jantar, menu.sobremesa, menu.bebida FROM pedidos INNER JOIN cliente ON pedidos.id_cliente = cliente.id INNER JOIN menu ON pedidos.id_menu = menu.id WHERE estado = 1";
-                using (MySqlCommand command1 = new MySqlCommand(query1, conn))
-                using (MySqlDataAdapter adapter = new MySqlDataAdapter(command1))
-                {
-                    DataTable dt = new DataTable();
-                    adapter.Fill(dt);
-                    repeaterOrdersAcepted.DataSource = dt;
-                    repeaterOrdersAcepted.DataBind();
-                }
-                conn.Close();
-
-                conn.Open();
-                string query2 = "SELECT pedidos.id, pedidos.data, cliente.nome, menu.entrada, menu.salada, menu.jantar, menu.sobremesa, menu.bebida FROM pedidos INNER JOIN cliente ON pedidos.id_cliente = cliente.id INNER JOIN menu ON pedidos.id_menu = menu.id WHERE estado = 0";
-                using (MySqlCommand command2 = new MySqlCommand(query2, conn))
-                using (MySqlDataAdapter adapter = new MySqlDataAdapter(command2))
-                {
-                    DataTable dt = new DataTable();
-                    adapter.Fill(dt);
-                    repeaterFinishedOrders.DataSource = dt;
-                    repeaterFinishedOrders.DataBind();
-                }
-
-            }
         }
 
         protected void btnAcept_Click(object sender, EventArgs e)
@@ -85,7 +55,6 @@ namespace Sistema_de_Gestão_Empresarial_Buffet_Divino_Sabor.Gestão.Pedidos
                 Log.Warning("Acabou");
 
             }
-
         }
 
         protected void btnFinish_Click(object sender, EventArgs e)
--- Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Negocio/pedidos.cs
+++ Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Negocio/pedidos.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
 
 namespace Sistema_de_Gestão_Empresarial_Buffet_Divino_Sabor.Negocio
 {
@@ -12,6 +13,78 @@ namespace Sistema_de_Gestão_Empresarial_Buffet_Divino_Sabor.Negocio
         public pedidos()
         {
             connection = new MySqlConnection(SiteMaster.ConnectionString);
+        }
+
+        public DataTable ReadNovos()
+        {
+            //cria uma tabela dt
+            DataTable dt = new DataTable();
+
+            //abre a conexão
+            connection.Open();
+            //comando MySQL para selecionar os dados
+            string query = "SELECT pedidos.id, pedidos.data, cliente.nome, menu.entrada, menu.salada, menu.jantar, menu.sobremesa, menu.bebida FROM pedidos INNER JOIN cliente ON pedidos.id_cliente = cliente.id INNER JOIN menu ON pedidos.id_menu = menu.id WHERE estado IS NULL";
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                //inclui os dados do banco de dados dentro de uma tabela 
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                {
+                    //encaixa os dados do adapter em uma tabela dt
+                    adapter.Fill(dt);
+                }
+                //fecha a conexão
+                connection.Close();
+            }
+            //retorna o valor da datatable
+            return dt;
+        }
+
+        public DataTable ReadAndamento()
+        {
+            //cria uma tabela dt
+            DataTable dt = new DataTable();
+
+            //abre a conexão
+            connection.Open();
+            //comando MySQL para selecionar os dados
+            string query = "SELECT pedidos.id, pedidos.data, cliente.nome, menu.entrada, menu.salada, menu.jantar, menu.sobremesa, menu.bebida FROM pedidos INNER JOIN cliente ON pedidos.id_cliente = cliente.id INNER JOIN menu ON pedidos.id_menu = menu.id WHERE estado = 1";
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                //inclui os dados do banco de dados dentro de uma tabela 
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                {
+                    //encaixa os dados do adapter em uma tabela dt
+                    adapter.Fill(dt);
+                }
+                //fecha a conexão
+                connection.Close();
+            }
+            //retorna o valor da datatable
+            return dt;
+        }
+
+        public DataTable ReadFinalizados()
+        {
+            //cria uma tabela dt
+            DataTable dt = new DataTable();
+
+            //abre a conexão
+            connection.Open();
+            //comando MySQL para selecionar os dados
+            string query = "SELECT pedidos.id, pedidos.data, cliente.nome, menu.entrada, menu.salada, menu.jantar, menu.sobremesa, menu.bebida FROM pedidos INNER JOIN cliente ON pedidos.id_cliente = cliente.id INNER JOIN menu ON pedidos.id_menu = menu.id WHERE estado =0 ";
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                //inclui os dados do banco de dados dentro de uma tabela 
+                using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                {
+                    //encaixa os dados do adapter em uma tabela dt
+                    adapter.Fill(dt);
+                }
+                //fecha a conexão
+                connection.Close();
+            }
+            //retorna o valor da datatable
+            return dt;
         }
 
         public bool Acept(Classe.pedidos pedidos)
