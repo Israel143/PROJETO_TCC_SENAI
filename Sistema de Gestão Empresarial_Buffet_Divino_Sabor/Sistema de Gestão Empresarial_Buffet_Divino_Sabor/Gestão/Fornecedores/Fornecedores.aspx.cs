9b5f5883636f3c3e086ccbb2adad44cce853eb05
LAB201 <LAB201@serveduc.com>
2023-02-27 22:22:51 -0300

    Implementação do Validar CPF e CNPJ e parte de Cadastrar e Pesquisar Fornecedores

---
+++ Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Gestão/Fornecedores/Fornecedores.aspx.cs
@@ -0,0 +1,54 @@
+﻿using MySqlConnector;
+using Sistema_de_Gestão_Empresarial_Buffet_Divino_Sabor.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sistema_de_Gestão_Empresarial_Buffet_Divino_Sabor.Gestão.Fornecedores
+{
+    public partial class Fornecedores : System.Web.UI.Page
+    {
+        private MySqlConnection conexao;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            conexao = new MySqlConnection(SiteMaster.ConnectionString);
+
+            if (!IsPostBack)
+            {
+
+            }
+        }
+
+        protected void btnCadastrar_Fornecedor_Click(object sender, EventArgs e)
+        {
+            //Cadastro de Fornecedores
+            var Fornecedores = new Classe.fornecedores();
+            Fornecedores.nome = txtNome_Fornecedor.Text;
+            Fornecedores.contato = txtcontato.Text;
+            Fornecedores.seguimento = txtSeguimento_Fornecedor.Text;
+            Fornecedores.cnpj = txtCNPJ.Text;
+            //Chama a função da pasta negocio para Criar um novo fornecedores
+            if (Utils.ValidarDocumento(txtCNPJ.Text))
+            {
+                var funcionarios = new Negocio.fornecedores().Create(Fornecedores);
+
+
+            }
+            SiteMaster.ExibirAlertRedirecionar(this, "Fornecedor Cadastrado com sucesso!", "Fornecedores.aspx");
+            conexao.Close();
+
+        }
+
+        protected void btnPesquisa_Fornecedor_Click(object sender, EventArgs e)
+        {
+            var Lista = new Negocio.fornecedores().Read(txtPesquisa.Text);
+
+            GridView1.DataSource = Lista;
+            GridView1.DataBind();
+
+        }
+    }
+}
--- Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Negocio/fornecedores.cs
+++ Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Sistema de Gestão Empresarial_Buffet_Divino_Sabor/Negocio/fornecedores.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,89 @@ namespace Sistema_de_Gestão_Empresarial_Buffet_Divino_Sabor.Negocio
 {
     public class fornecedores
     {
+        private MySqlConnection conexao;
+        public fornecedores()
+        {
+
+            conexao = new MySqlConnection(SiteMaster.ConnectionString);
+
+
+        }
+        public bool Create(Classe.fornecedores Fornecedores)
+        {
+            conexao.Open();
+            try
+            {
+                var comando = new MySqlCommand("INSERT INTO fornecedores  (nome,contato,seguimento,cnpj) VALUES (@nome,@contato,@seguimento,@cnpj)", conexao);
+
+                comando.Parameters.Add(new MySqlParameter("nome", Fornecedores.nome));
+                comando.Parameters.Add(new MySqlParameter("contato", Fornecedores.contato));
+                comando.Parameters.Add(new MySqlParameter("seguimento", Fornecedores.seguimento));
+                comando.Parameters.Add(new MySqlParameter("cnpj", Fornecedores.cnpj));
+              
+                comando.ExecuteNonQuery();
+            }
+            catch
+            {
+                return false;
+            }
+
+
+            finally
+            {
+                conexao.Close();
+            }
+
+            return true;
+
+        }
+
+        public List<Classe.fornecedores> Read(string fornecedores)
+        {
+
+
+
+            var Lista = new List<Classe.fornecedores>();
+            try
+
+            {
+                conexao.Open();
+                var reader = new MySqlCommand("SELECT id,nome,contato,seguimento,cnpj FROM fornecedores WHERE nome  like '%" + fornecedores + "%'", conexao).ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var novo_fornecedores = new Classe.fornecedores();
+                    novo_fornecedores.id = reader.GetInt32("id");
+                    novo_fornecedores.nome = reader.GetString("nome");
+                    novo_fornecedores.contato = reader.GetString("contato");
+                    novo_fornecedores.seguimento = reader.GetString("seguimento");
+                    novo_fornecedores.cnpj = reader.GetString("cnpj");
+                    Lista.Add(novo_fornecedores);
+
+                }
+            }
+            catch
+            {
+
+
+            }
+            finally
+            {
+
+                conexao.Close();
+
+            }
+
+            return Lista;
+
+
+
+
+        }
+
+
+
+
     }
 }
+    
